a101abb4b59391e626d5c33b45db2338e6f681b2
dogsdiarrhea <123253407+dogsdiarrhea@users.noreply.github.com>
2023-01-21 15:29:52 +0000

    Initial commit

---
+++ views/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return <>
+			<link rel="stylesheet" href="/public/styles.css" />
+      <div className="App">
+        <header className="App-header">
+           Hello world
+          
+        </header>
+      </div>
+    </>;
+  }
+}
+
+export default App;
